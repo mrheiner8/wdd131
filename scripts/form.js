7452da7c35db6f0459bb9e80381d7f2afb6e7580
mrheiner8 <mrheiner8@gmail.com>
2026-06-08 23:10:06 -0600

    save it

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,42 @@
 // Select the HTML elements
 const today = new Date();
 const currentYear = today.getFullYear();
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+const productSelect = document.getElementById("product-name");
+
+products.forEach(product => {
+    let newOption = document.createElement("option");
+    newOption.value = product.id;
+    newOption.textContent = product.name;
+    productSelect.appendChild(newOption);
+
+});
 
 document.getElementById("currentYear").innerHTML = currentYear;
 
