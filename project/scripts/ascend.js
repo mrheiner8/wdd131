4d58bfc6ee6029349ef5d9d327550f735f3fcc54
mrheiner8 <mrheiner8@gmail.com>
2026-06-12 22:05:05 -0600

    menu

--- project/scripts/ascend.js
+++ project/scripts/ascend.js
@@ -1,8 +1,16 @@
-// Get current date data for the footer
+// 1. Select the elements
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
 const today = new Date();
 const currentYear = today.getFullYear();
 
-
-// Inject the footer dates into the HTML spans
+// 2. Add the click event listener
+menuButton.addEventListener('click', () => {
+    // 3. Toggle the 'show' class on and off
+    navigation.classList.toggle('show');
+    // 4.Optional: Change the button symbol from ≡ to X when open
+    menuButton.textContent = navigation.classList.contains('show') ? 'X' : '≡';
+});
+// 5.Inject the footer dates into the HTML spans
 document.getElementById("currentYear").innerHTML = currentYear;
 document.getElementById("lastModified").innerHTML = document.lastModified;
