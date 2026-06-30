9ba941d038e941d346cba73bcdbbfabe09d6e131
mrheiner8 <mrheiner8@gmail.com>
2026-06-30 14:14:48 -0600

    nothing really

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,12 @@
     <p>©<span id="currentyear"></span> 🎷Michael Heiner 🎷 Colorado</p>
     <p>Last Modification: <span id="lastModified"></span></p>
 </footer>
+
+
+// Get current date data for the footer
+const today = new Date();
+const currentYear = today.getFullYear();
+
+// Inject the footer dates into the HTML spans
+document.getElementById("currentYear").innerHTML = currentYear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
